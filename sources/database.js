ddba9d3cd11aa55873e092ff03de47a905166b40
Triptpal Singh <triptpalsingh721@gmail.com>
2021-05-15 00:26:04 +0530

    tried

---
+++ sources/database.js
@@ -0,0 +1,14 @@
+var database ={
+            "data":[
+                {
+                    "email":"admin",
+                    "password":"admin"
+                },
+                {
+                    "email":"triptpalsingh@gmail.com",
+                    "password":"1234Tript@"
+                }
+            ]
+        };
+
+localStorage.setItem("loginData", JSON.stringify(database));
